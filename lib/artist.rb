41fc2dd27a4efbdcc0ce187f407006e029814582
Becky Rosa <becky_m_rosa@yahoo.com>
2021-05-16 19:07:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,7 +9,11 @@ def initialize(name)
   @@all << self
 end
 
-def self.all
-  @@all
-end
+  def self.all
+    @@all
+  end
+
+  def new_song(name, genre)
+    Song.new(self, name, genre)
+  end
 end
